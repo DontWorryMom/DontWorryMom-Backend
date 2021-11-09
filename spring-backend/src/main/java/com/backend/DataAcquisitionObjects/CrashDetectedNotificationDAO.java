dc03948355bbaeb5e590a0529e4e05b315a131b6
William Sun <sunwilliam@gmail.com>
2021-11-08 21:26:48 -0800

    Added to CrashDetectedNotificationDAO

--- spring-backend/src/main/java/com/backend/DataAcquisitionObjects/CrashDetectedNotificationDAO.java
+++ spring-backend/src/main/java/com/backend/DataAcquisitionObjects/CrashDetectedNotificationDAO.java
@@ -1,8 +1,11 @@
 package com.backend.DataAcquisitionObjects;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.backend.Models.CrashDetectedNotification;
+import com.backend.Models.CrashDetectedNotificationId;
+import com.backend.Models.NotificationStatus;
 import com.backend.Repositories.CrashDetectedNotificationRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +21,40 @@ public class CrashDetectedNotificationDAO {
 		this.cdnRepo = cdnRepo;
 	}
 
+	/*
+	 *	READ DATA
+	 */
+
 	public List<CrashDetectedNotification> getCrashDetectedNotificationList() {
 		return cdnRepo.findAll();
 	}
+
+	public List<CrashDetectedNotification> getCrashDetectedNotificationListByLocationId(long locationId) {
+		return cdnRepo.findAllByLocationId(locationId);
+	}
+
+	/*
+	 *	CREATE DATA
+	 */
+
+	public CrashDetectedNotification createCrashDetectedNotification(long locationId, long notificationId) {
+		CrashDetectedNotification cdn = new CrashDetectedNotification(locationId, notificationId, NotificationStatus.PENDING);
+		return cdnRepo.save(cdn);
+	}
+
+	/*
+	 *	UPDATE DATA
+	 */
+
+	public CrashDetectedNotification updateCrashDetectedNotificationStatus(long locationId, long notificationId, NotificationStatus status) {
+		Optional<CrashDetectedNotification> cdnOptional = cdnRepo.findById(new CrashDetectedNotificationId(locationId, notificationId));
+		if(cdnOptional.isPresent()) {
+			CrashDetectedNotification cdn = cdnOptional.get();
+			cdn.setNotificationStatus(status);
+			return cdnRepo.save(cdn);
+		} else {
+			throw new IllegalArgumentException("Could not find CrashDetectedNotification with locationId "+locationId+" and notificationId "+notificationId);
+		}
+	}
+
 }
--- spring-backend/src/main/java/com/backend/Repositories/CrashDetectedNotificationRepository.java
+++ spring-backend/src/main/java/com/backend/Repositories/CrashDetectedNotificationRepository.java
@@ -1,10 +1,12 @@
 package com.backend.Repositories;
 
+import java.util.List;
+
 import com.backend.Models.CrashDetectedNotification;
 import com.backend.Models.CrashDetectedNotificationId;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrashDetectedNotificationRepository extends JpaRepository<CrashDetectedNotification, CrashDetectedNotificationId> {
-	
+	public List<CrashDetectedNotification> findAllByLocationId(long locationId);
 }
