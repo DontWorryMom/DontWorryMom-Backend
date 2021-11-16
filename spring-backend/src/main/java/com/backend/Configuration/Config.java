5fd818614225f9ae1692718be86630aa80451113
William Sun <sunwilliam@gmail.com>
2021-11-16 12:13:19 -0800

    Removed kafka due to time constraints

--- spring-backend/src/main/java/com/backend/Configuration/Config.java
+++ spring-backend/src/main/java/com/backend/Configuration/Config.java
@@ -7,7 +7,6 @@ import java.util.Map.Entry;
 public class Config {
 	private static final Map<String,String> ENVIRONMENT_VARIABLES = System.getenv(); 
 
-	public final static String KAFKA_HOST = ENVIRONMENT_VARIABLES.getOrDefault("KAFKA_HOST", "kafka:29092");
 	public final static String TWILIO_ACCOUNT_SID = ENVIRONMENT_VARIABLES.get("TWILIO_ACCOUNT_SID");
 	public final static String TWILIO_AUTH_TOKEN = ENVIRONMENT_VARIABLES.get("TWILIO_AUTH_TOKEN");
 	public final static String TWILIO_FROM_NUMBER = ENVIRONMENT_VARIABLES.get("TWILIO_PHONE_NUMBER");
@@ -19,7 +18,6 @@ public class Config {
 
 	public static void printConfig() {		
 		Map<String,Object> configVariables = new HashMap<>();
-		configVariables.put("kafka-host", KAFKA_HOST);
 		configVariables.put("twilio-account-sid", TWILIO_ACCOUNT_SID);
 		configVariables.put("twilio-auth-token", TWILIO_AUTH_TOKEN);
 		configVariables.put("twilio-from-number", TWILIO_FROM_NUMBER);
